fb8345facebfe9b6b099167b59a89373447c9173
Ahmed Abu zaid <ahmedabuzaid@Ahmeds-MacBook-Pro.local>
2019-01-08 17:07:56 +0200

    add post users/login route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _ = require ("lodash");
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
@@ -98,10 +99,7 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
-  var user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
+  var user = new User(body);
 
   user.save().then(() => {
     return user.generateAuthToken();
@@ -116,6 +114,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+   var body = _.pick(req.body, ['email', 'password']);
+
+   User.findByCredentials(body.email, body.password).then((user) => {
+    return  user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+   }).catch((e) => {
+     res.status(400).send();
+   });
 });
 
 app.listen(port, () => {
